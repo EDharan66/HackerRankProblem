66ebfaaae716d749f7850fcd0dc65f801eecfc08
Hariharan <hariharan80122@gmail.com>
2021-07-22 16:54:47 +0530

    commit_4

---
+++ OrganizingContainersOfBalls/OrganizingContainersOfBalls.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result5 {
+
+    /*
+     * Complete the 'organizingContainers' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts 2D_INTEGER_ARRAY container as parameter.
+     */
+
+    public static String organizingContainers(List<List<Integer>> asList) {
+        // Write your code here
+        long[] container = new long[asList.size()];
+        long[] convertedBall = new long[asList.size()];
+        for (int i = 0; i < asList.size(); i++) {
+            long tempContainerSum = 0;
+            long tempBall = 0;
+            for (int j = 0; j < asList.get(i).size(); j++) {
+                tempContainerSum = tempContainerSum + asList.get(i).get(j);
+                tempBall = tempBall + asList.get(j).get(i);
+            }
+            convertedBall[i] = tempBall;
+            container[i] = tempContainerSum;
+        }
+
+        Arrays.sort(container);
+        Arrays.sort(convertedBall);
+
+
+        if (Arrays.equals(container, convertedBall)) {
+            return "Possible";
+        } else {
+            return "Impossible";
+        }
+    }
+
+}
+
+public class OrganizingContainersOfBalls {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<List<Integer>> container = new ArrayList<>();
+
+                IntStream.range(0, n).forEach(i -> {
+                    try {
+                        container.add(
+                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                        .map(Integer::parseInt)
+                                        .collect(toList())
+                        );
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                String result = Result5.organizingContainers(container);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ OrganizingContainersOfBalls/Readme.md
@@ -0,0 +1,3 @@
+![Alt text](/ProblemStatement/organizing-containers-of-balls-English_page-0001.jpg)
+![Alt text](/ProblemStatement/organizing-containers-of-balls-English_page-0002.jpg)
+![Alt text](/ProblemStatement/organizing-containers-of-balls-English_page-0003.jpg)
